bbf8e488783a09b0af49a7a77f0943fd7624deb1
Nikolay <n.g.stakovichenko@gmail.com>
2023-01-25 21:24:19 +0200

    feat: adaptive 380px + burger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-"use strict"
+// "use strict"
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.navigation'),
+    menuItem = document.querySelectorAll('.navigation__link'),
+    hamburger = document.querySelector('.hamburger');
 
-const hamburger = document.querySelector('.')
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger__active');
+        menu.classList.toggle('navigation__active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger__active');
+            menu.classList.toggle('navigation__active');
+        })
+    })
+})
+
+// const hamburger = document.querySelector('.')
